f5e00f9ac72575b29c8eec9a32a64e5bc88f7e7b
Bhargav Bhalodiya <your-email@gmail.com>
2026-06-10 12:42:13 +0530

    Push

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    allowedDevOrigins: ["13.234.200.90", "13.234.200.90:3500"],
+  },
+};
 
 export default nextConfig;
+
